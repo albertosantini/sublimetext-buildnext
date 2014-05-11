703d5ecd8becc952c9587ec6f29113a416855d7a
icebox <albertosantini@gmail.com>
2014-05-11 13:30:49 +0200

    Fix flake8 and pep257 errors.

--- BuildNext.py
+++ BuildNext.py
@@ -1,3 +1,7 @@
+"""
+This package augments the default build system.
+"""
+
 import sublime
 import sublime_plugin
 import re
@@ -7,8 +11,17 @@ defaultExec = importlib.import_module("Default.exec")
 
 output_errors = {}
 
+
 class ExecCommand(defaultExec.ExecCommand):
+    """
+    This class extends the default build system.
+    """
+
     def on_finished(self, proc):
+        """
+        It is the entry point after the process is finished.
+        """
+
         global output_errors
 
         super(ExecCommand, self).on_finished(proc)
@@ -17,7 +30,7 @@ class ExecCommand(defaultExec.ExecCommand):
         output_view = self.output_view
 
         key = sublime.active_window().active_view().file_name()
-        key.replace("\\","/")
+        key.replace("\\", "/")
 
         if (len(output_view.find_all_results()) == 0 and proc.exit_code() == 0):
             sublime.active_window().run_command("hide_panel", {"cancel": True})
@@ -29,13 +42,22 @@ class ExecCommand(defaultExec.ExecCommand):
             output_errors[key] = self.getErrors(output_view)
 
             regions = output_errors[key]["error_regions"]
-            view.add_regions("exec_errors", regions, "keyword", "dot",
-                    sublime.DRAW_EMPTY_AS_OVERWRITE |
+            view.add_regions(
+                "exec_errors",
+                regions,
+                "keyword",
+                "dot",
+                sublime.DRAW_EMPTY_AS_OVERWRITE |
                     sublime.DRAW_NO_FILL |
                     sublime.DRAW_NO_OUTLINE |
-                    sublime.HIDE_ON_MINIMAP)
+                    sublime.HIDE_ON_MINIMAP
+            )
 
     def getAdjustedRegion(self, line, col):
+        """
+        It adjust the line and column values if the view contains tabs.
+        """
+
         line = int(line) - 1
         view = self.window.active_view()
         settings = view.settings()
@@ -53,6 +75,10 @@ class ExecCommand(defaultExec.ExecCommand):
         return sublime.Region(text_point, text_point)
 
     def getErrors(self, view):
+        """
+        It parse the output of the build system to get the errors.
+        """
+
         view_errors = {
             "view": view,
             "view_text": view.substr(sublime.Region(0, view.size())),
@@ -87,20 +113,41 @@ class ExecCommand(defaultExec.ExecCommand):
 
         return view_errors
 
+
 class ReplaceTextOutputView(sublime_plugin.TextCommand):
+    """
+    It replaces the text in the output view.
+    """
+
     def run(self, edit, args):
-        self.view.replace(edit, sublime.Region(0, self.view.size()),
-            args["text"])
+        """
+        Run the text command.
+        """
+
+        self.view.replace(
+            edit,
+            sublime.Region(0, self.view.size()),
+            args["text"]
+        )
+
 
 class GotoError(sublime_plugin.TextCommand):
+    """
+    It is the helper class to go to the error.
+    """
+
     def run(self, edit, direction):
+        """
+        Run the text command.
+        """
+
         global output_errors
 
         key = sublime.active_window().active_view().file_name()
         if (not key):
             return
 
-        key.replace("\\","/")
+        key.replace("\\", "/")
 
         if (key not in output_errors):
             return
@@ -127,33 +174,73 @@ class GotoError(sublime_plugin.TextCommand):
             err_region_end = err_region.end()
             if ((direction == "next" and (caret < err_region_end)) or
                 (direction == "prev" and (caret > err_region_end))):
-                self.updateEditAndOutputView(output_view,
-                    error_regions[i], error_messages[i], output_regions[i])
+                self.updateEditAndOutputView(
+                    output_view,
+                    error_regions[i],
+                    error_messages[i],
+                    output_regions[i]
+                )
                 break
         else:
-            self.updateEditAndOutputView(output_view,
-                error_regions[0], error_messages[0], output_regions[0])
+            self.updateEditAndOutputView(
+                output_view,
+                error_regions[0],
+                error_messages[0],
+                output_regions[0]
+            )
 
     def updateEditAndOutputView(self, view, region, message, output):
+        """
+        It updated the edit and output view.
+        """
+
         self.highlightBuildError(view, output)
         sublime.status_message(message)
         self.setCaret(self.view, region)
 
     def setCaret(self, view, position):
+        """
+        It sets the caret.
+        """
+
         view.sel().clear()
         view.sel().add(sublime.Region(position.begin(), position.end()))
         view.show_at_center(position.end())
 
     def highlightBuildError(self, view, position):
+        """
+        It highlights the line error in the output view.
+        """
+
         self.setCaret(view, position)
         sublime.active_window().run_command("hide_panel", {"cancel": True})
-        sublime.active_window().run_command("show_panel",
-            {"panel": "output.exec"})
+        sublime.active_window().run_command(
+            "show_panel",
+            {"panel": "output.exec"}
+        )
+
 
 class GotoNextError(GotoError):
+    """
+    It maps the key binding to go to the next error.
+    """
+
     def run(self, edit):
+        """
+        Run the text command.
+        """
+
         super(GotoNextError, self).run(edit, "next")
 
+
 class GotoPrevError(GotoError):
+    """
+    It maps the key binding to go to the previous error.
+    """
+
     def run(self, edit):
+        """
+        Run the text command.
+        """
+
         super(GotoPrevError, self).run(edit, "prev")
